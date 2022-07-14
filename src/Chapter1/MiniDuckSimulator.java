4d9d0e51dce874c4d6e872e042b045e4594f6fe4
Jinuk Lee <20181680@edu.hanbat.ac.kr>
2022-07-14 12:50:04 +0900

    디자인 패턴 1장 전략 패턴

--- src/Chapter1/MiniDuckSimulator.java
+++ src/Chapter1/MiniDuckSimulator.java
@@ -6,11 +6,11 @@ public class MiniDuckSimulator {
 //        mallard.performQuack();
 //        mallard.performFly();
 
-        System.out.println("동적 할당 전");
+        System.out.println("동적 할당 하기 전");
         Duck model = new ModelDuck();
         model.performFly();
         System.out.println();
-        System.out.println("동적 할당 후");
+        System.out.println("동적 할당 한 후");
         model.setFlyBehavior(new FlyRocketPowered());
         model.performFly();
     }
