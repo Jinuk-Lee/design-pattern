73ce20451c5c9b60ad6e59b0fbfd570740049e01
jinuklee <20181680@edu.hanbat.ac.kr>
2022-07-16 16:18:54 +0900

    라이브러리 속 옵저버 패턴 알아보기

---
+++ src/Chapter2/SwingObserverExample.java
@@ -0,0 +1,29 @@
+package Chapter2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class SwingObserverExample {
+	JFrame frame;
+	public static void main(String[] args) {
+		SwingObserverExample example = new SwingObserverExample();
+		example.go();
+	}
+	public void go() {
+		frame = new JFrame();
+		JButton button = new JButton("할까? 말까?");
+		button.addActionListener(new AngelListener());
+		button.addActionListener(new DevilListener());
+		// 프레임 속성을 설정합니다.
+	}
+	class AngelListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("하지 마! 아마 후회할 걸?");
+		}
+	}
+	class DevilListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("그냥 저질러 버렷!!!");
+		}
+	}
+}
