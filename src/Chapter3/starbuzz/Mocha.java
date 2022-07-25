66e2f2bbbeeb2c4499fdfee673d88dbaf1d7a7c5
jinuklee <20181680@edu.hanbat.ac.kr>
2022-07-25 17:26:57 +0900

    첨가물 코드 구현하기

---
+++ src/Chapter3/starbuzz/Mocha.java
@@ -0,0 +1,15 @@
+package Chapter3.starbuzz;
+
+public class Mocha extends CondimentDecorator {
+	public Mocha(Beverage beverage) {
+		this.beverage = beverage;
+	}
+ 
+	public String getDescription() {
+		return beverage.getDescription() + ", 모카";
+	}
+ 
+	public double cost() {
+		return .20 + beverage.cost();
+	}
+}
