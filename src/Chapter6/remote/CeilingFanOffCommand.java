0bf76ca0739feeb5f890dc7c3d2ecbf78ce52464
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-09 16:49:17 +0900

    리모컨 코드 구현하기

---
+++ src/Chapter6/remote/CeilingFanOffCommand.java
@@ -0,0 +1,12 @@
+package Chapter6.remote;
+
+public class CeilingFanOffCommand implements Command {
+	CeilingFan ceilingFan;
+
+	public CeilingFanOffCommand(CeilingFan ceilingFan) {
+		this.ceilingFan = ceilingFan;
+	}
+	public void execute() {
+		ceilingFan.off();
+	}
+}
---
+++ src/Chapter6/remote/NoCommand.java
@@ -0,0 +1,5 @@
+package Chapter6.remote;
+
+public class NoCommand implements Command {
+	public void execute() { }
+}
